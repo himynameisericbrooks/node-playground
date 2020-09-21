df10293c950ac4518227ff45ec2dc11064cd4b1e
Eric Brooks <ericbrookspro@gmail.com>
2020-09-20 23:24:58 -0500

    requests without a library practice

---
+++ 6-raw-http.js
@@ -0,0 +1,23 @@
+const https = require('https')
+
+const url = 'http://api.weatherstack.com/current?access_key=56bbc6f76fa91121aae7a4a8ab3b0466&query=45,-75&units=f'
+
+const request = https.request(url, (response) => {
+    let data = ''
+
+    response.on('data', (chunk) => {
+        data = data + chunk.toString()
+    })
+
+    response.on('end', () => {
+    const body = JSON.parse(data)
+    console.log(body)
+    })
+
+})
+
+request.on('error', (error) => {
+    console.log('An error', error)
+})
+
+request.end()
